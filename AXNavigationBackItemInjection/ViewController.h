5012ae6920f72aae71914b899df4753b72895eb5
aiXing <862099730@qq.com>
2016-08-03 11:09:11 +0800

    Initial Commit

---
+++ AXNavigationBackItemInjection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXNavigationBackItemInjection
+//
+//  Created by devedbox on 16/8/3.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AXNavigationBackItemInjection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AXNavigationBackItemInjection
+//
+//  Created by devedbox on 16/8/3.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
